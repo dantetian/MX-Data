6c300eb3646a078299fdcad199752e43c0a18998
dantetian <353341086@qq.com>
2017-03-12 17:04:56 +0800

    修改gen_pbc.bat

--- gen_pbc.bat
+++ gen_pbc.bat
@@ -1,7 +1,8 @@
-git pull
 
 protoc P_Asset.proto -o P_Asset.pb
 protoc P_Protocol.proto -o P_Protocol.pb
 
-copy /Y *.pb ..\MJProject\Assets\Scripts\LuaScripts\Resources 
+copy /Y *.pb ..\MJProject\Assets\StreamingAssets
+copy /Y Asset ..\MJProject\Assets\StreamingAssets\Asset
+copy /Y *P_Asset.proto ..\MJProject\Assets\StreamingAssets\Asset\Asset
 pause
